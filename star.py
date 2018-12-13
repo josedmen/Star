8312c83f3d8bad934fc8323512e034d29f4c0817
Jose Daniel Mendez <45828094+josedmen@users.noreply.github.com>
2018-12-13 09:52:33 +0000

    Add files via upload

---
+++ star.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+
+speed(1)
+pensize(8)
+pencolor("yellow")
+fillcolor("yellow")
+begin_fill()
+forward(200)
+left(144)
+forward(200)
+left(144)
+forward(200)
+left(144)
+forward(200)
+left(144)
+forward(200)
+left(144)
+end_fill()
+
+exitonclick()
+
+
